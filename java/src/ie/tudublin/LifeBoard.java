e004d928e7b6e32980031db5d463190ad9d0da5f
Niamh <C22397066@mytudublin.ie>
2024-02-19 12:50:57 +0000

    second part

--- java/src/ie/tudublin/LifeBoard.java
+++ java/src/ie/tudublin/LifeBoard.java
@@ -26,14 +26,61 @@ public class LifeBoard {
 
     }
 
+    public void randomize()
+    {
+        for(int row = 0; row < rows; row ++)
+        {
+            for(int col = 0; col < cols; col ++)
+            {
+                float dice = p.random(1.0f);
+                board[rows][cols] = (dice < 0.5f);
+            }
+        }
+    }
+
     public void update()
     {
 
     }
 
+    public void setCell(int row, int col, boolean value)
+    {
+        if(row > 0 && col > 0 && row < rows && col < cols)
+        {
+            board[row][col] = value;
+        }
+    }
+
+    public boolean getCell(int row, int col)
+    {
+        if(row > 0 && col > 0 && row < rows && col < cols)
+        {
+            board[row][col] = value; //needs to be fixed
+        }
+        return false;
+    }
+
     public void render()
     {
-        
+        for(int row = 0 ; row < rows ; row ++)
+        {
+            for(int col = 0 ; col < cols ; col ++)
+            {
+                float x = p.map(col, 0, cols, 0, p.width);
+                float y = row * cellHeight;
+                p.stroke(0,255, 255);
+                if (board[row][col])
+                {
+                    p.fill(0, 255, 0);
+                }
+                else
+                {
+                    p.noFill();
+                }
+                p.rect(x, y, cellWidth, cellHeight);
+            }           
+        }  
+
     }
     
 }
